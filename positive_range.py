c5883f646d1c2d38a3bead9a974d3166d81334ae
samarthp07 <82090424+samarthp07@users.noreply.github.com>
2021-04-16 13:45:50 +0530

    Uploaded by Samarth Penshanwar
    This program prints only positive number from provided list...

---
+++ positive_range.py
@@ -0,0 +1,16 @@
+
+print ("code for printing positive no. within a range!")
+print("_____________________________________")
+start = int(input("Enter the start of range: ")) 
+
+end = int(input("Enter the end of range: ")) 
+
+for num in range(start, end + 1): 
+
+      
+
+    # checking condition 
+
+    if num >= 0: 
+
+        print(num, end = " ") 
